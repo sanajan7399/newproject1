fd0630353cd8cd2e03f140cf0090d2d6a1c85539
user <user@DESKTOP-ISDQQNT>
2023-10-18 15:13:53 +0530

    new1

--- newproject/src/main/java/newproject/new1.java
+++ newproject/src/main/java/newproject/new1.java
@@ -4,7 +4,7 @@ public class new1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hello world");
+System.out.println("hello world");//jenk
 	}
 
 }
